e2cfd1b6d798eeb2dded7e6beb8e5e8577c98065
Josephine Emenya <115475933+josieemenya@users.noreply.github.com>
2025-09-01 10:43:17 +0100

    this was an update to many things, i think

--- main.cpp
+++ main.cpp
@@ -23,7 +23,7 @@ int main(int argc, char* argv[]) {
     return 1;
     }
 
-    SDL_Surface* WindowsIcon = SDL_LoadBMP("chip8windowsICON.bmp");
+   
 
     Platform platform("CHIP-8 Emulator", VIDEO_WIDTH * videoScale, VIDEO_HEIGHT * videoScale, VIDEO_WIDTH, VIDEO_HEIGHT);
 
